3a4ac8f4d0be4694157811789e8c3f6b8868ac9b
Hy Huynh <hyxhuynh@gmail.com>
2017-12-01 23:12:03 -0800

    Initial commit of bank app

---
+++ BankApp/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This project is to create a bank account.
+    /// You can write a summary in multiple lines.
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        public int AccountNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string AccountName { get; set; }
+        public string AcountType { get; set; }
+        public decimal Balance { get; set; }
+        #endregion
+    }
+}
